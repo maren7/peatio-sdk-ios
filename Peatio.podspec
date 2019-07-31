98427a371aea3d451ae7c9770de0e9ba7a3952e3
wsof401 <oct.song.wu@gmail.com>
2019-07-31 14:18:48 +0800

    bump

--- Peatio.podspec
+++ Peatio.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |spec|
 
   spec.name         = "Peatio"
-  spec.version      = "0.7.0"
+  spec.version      = "0.7.1"
   spec.summary      = "PeatioSDK iOS version."
   spec.homepage     = "https://github.com/peatio/peatio-sdk-ios"
   spec.license      = "LICENSE"
--- PeatioSDK/API/HttpClient/Error/APIError.swift
+++ PeatioSDK/API/HttpClient/Error/APIError.swift
@@ -16,6 +16,10 @@ public struct APIError: LocalizedError, Hashable {
         }
         return result
     }
+
+    public static func generate(by code: APIError.Code) -> APIError {
+        return APIError(code: code.rawValue, message: "", response: nil, data: nil)
+    }
 }
 
 extension APIError: CustomDebugStringConvertible, CustomStringConvertible {
