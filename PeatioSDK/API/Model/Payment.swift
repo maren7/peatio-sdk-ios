360cfc20bb79c962e2152e012ad2339048187665
wsof401 <oct.song.wu@gmail.com>
2019-09-05 12:15:01 +0800

    bump

--- PeatioSDK/API/Model/Payment.swift
+++ PeatioSDK/API/Model/Payment.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 public struct Payment: Codable {
+    public let id: Int
     public let method: PaymentMethodType
     public let account: String
     public let accountName: String
