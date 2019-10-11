d22cec23768fe989ab9f67d929a4532806052e43
wsof401 <oct.song.wu@gmail.com>
2019-10-11 14:48:47 +0800

    bump

--- PeatioSDK/API/Model/OrderPatch.swift
+++ PeatioSDK/API/Model/OrderPatch.swift
@@ -16,4 +16,22 @@ public struct OrderPatch {
     public let insertedAt: Date
     public let side: OrderSide
     public let state: State
+
+    public init(id: String,
+                assetPairName: String,
+                price: String,
+                amount: String,
+                filledAmount: String,
+                insertedAt: Date,
+                side: OrderSide,
+                state: State) {
+        self.id = id
+        self.assetPairName = assetPairName
+        self.price = price
+        self.amount = amount
+        self.filledAmount = filledAmount
+        self.insertedAt = insertedAt
+        self.side = side
+        self.state = state
+    }
 }
