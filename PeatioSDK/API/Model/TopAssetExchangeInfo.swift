5db1ec27752b363f4655a1bed8d73e7960f64729
wsof401 <oct.song.wu@gmail.com>
2019-10-31 12:33:40 +0800

    bump

--- PeatioSDK/API/Model/TopAssetExchangeInfo.swift
+++ PeatioSDK/API/Model/TopAssetExchangeInfo.swift
@@ -1,9 +1,19 @@
 import Foundation
 
 public struct TopAssetExchangeInfo: Codable {
+
+    private enum CodingKeys: String, CodingKey {
+        case assetUUID = "assetUuid"
+        case volume
+        case fiatSymbol
+        case fiatVolume
+        case maxVolumeAssetPair
+    }
+
+
     public let assetUUID: String
     public let volume: String
     public let fiatSymbol: String
     public let fiatVolume: String
-    public let maxVolumeAssetPaire: AssetPair
+    public let maxVolumeAssetPair: AssetPair
 }
