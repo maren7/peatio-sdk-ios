a1a1457a196435a1ef3091e60ee181c98d296b11
wsof401 <oct.song.wu@gmail.com>
2019-12-06 00:59:11 +0800

    bump

--- Peatio.podspec
+++ Peatio.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |spec|
 
   spec.name         = "Peatio"
-  spec.version      = "0.9.47"
+  spec.version      = "0.9.48"
   spec.summary      = "PeatioSDK iOS version."
   spec.homepage     = "https://github.com/peatio/peatio-sdk-ios"
   spec.license      = "LICENSE"
--- PeatioSDK/API/Operations/HTTPRequest/UserCenter/Utilities/GetVerificationsOperation.swift
+++ PeatioSDK/API/Operations/HTTPRequest/UserCenter/Utilities/GetVerificationsOperation.swift
@@ -5,6 +5,10 @@ public final class GetVerificationsOperation: RequestOperation {
 
     public let path: String = "/api/uc/v2/verifications"
 
+    public var additionalHeaders: [String : String?]? {
+           return ["x-validation": param.hciCode]
+       }
+
     public var requestParams: [String: Any?]? {
         return ["type": param.type.rawValue,
                 "nation_code": param.nationCode,
@@ -24,13 +28,16 @@ public extension GetVerificationsOperation {
         public let type: VerificationType
         public let nationCode: String?
         public let identity: String?
+        public let hciCode: String?
 
         public init(type: VerificationType,
                     nationCode: String?,
-                    identity: String?) {
+                    identity: String?,
+                    hciCode: String? = nil) {
             self.type = type
             self.nationCode = nationCode
             self.identity = identity
+            self.hciCode = hciCode
         }
     }
 }
