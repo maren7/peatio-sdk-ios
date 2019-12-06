987c403ca7a450158b2c45e0c3608be3578a809d
wsof401 <oct.song.wu@gmail.com>
2019-12-06 18:32:31 +0800

    bump

---
+++ PeatioSDK/API/Operations/HTTPRequest/App/HomeLayoutOperation.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+public final class HomeLayoutOperation: RequestOperation {
+    public typealias ResultData = HomeLayoutOperation.Result
+
+    public var path: String = "api/lego/v1/config/app_home"
+
+    public let param: Param
+
+    public var requestParams: [String : Any?]? {
+        return [
+            "client_type": "IOS",
+        ]
+    }
+
+    public init(param: Param) {
+        self.param = param
+    }
+}
+
+public extension HomeLayoutOperation {
+
+    struct Result: Decodable {
+        public struct Entry: Decodable {
+            public let title: String
+            public let iconUrl: String
+            public let linkUrl: String
+        }
+
+        public struct Config: Decodable {
+            public let especiallyAssetPairUuids: [String]
+            public let quickEntries: [Entry]
+        }
+
+        public let configs: Config
+    }
+
+    struct Param: Equatable {
+        public init() { }
+    }
+}
